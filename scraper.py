07b3cb050013f70f86512e01c970159278c2b5ab
unknown <vishal170997@gmail.com>
2022-04-12 20:48:06 +0530

    unwanted file removed

--- scraper.py
+++ scraper.py
@@ -1,9 +1,5 @@
-import os
-import time
+
 import requests
-import selenium
-import time
-import io
 import pandas as pd
 import streamlit as st
 import instagramy
@@ -16,11 +12,10 @@ from bs4 import BeautifulSoup
 
 def app():
     st.header("Welcome to iNeuron scrapper!!")
-    platform = st.sidebar.selectbox("Select the platform you want to scrape",
-                                    ('Instagram', 'Naukari', 'Google', 'Baidu'))
+    platform = st.sidebar.selectbox("Select the platform you want to scrape", ('Instagram', 'Naukari', 'Stack Overflow', 'Baidu'))
 
+# Instagram Scrapper
     try:
-        # Instagram Scrapper
         if platform == 'Instagram':
             user = st.text_input("Enter the instagram id you want to scrape..")
             user = instagramy.InstagramUser(user)
@@ -41,14 +36,13 @@ def app():
         return e
 
     # Naukari Scrapper
-
-
     try:
         if platform == 'Naukari':
             job = st.text_input("Enter the job you want to apply for:").replace(' ', '')
             location = st.text_input("Enter the location you want to apply for:").replace(' ', '')
             df = pd.DataFrame(
-                columns=['Job Title', 'Job URL', 'Company Title', 'Company URL', 'Ratings', 'Reviews', 'Experience','Salary', 'Location', 'Job_Post_History'])
+                columns=['Job Title', 'Job URL', 'Company Title', 'Company URL', 'Ratings', 'Reviews', 'Experience',
+                         'Salary', 'Location', 'Job_Post_History'])
             url = f"https://www.naukri.com/{job}-jobs-in-{location}"
             page = requests.get(url)
             driver = webdriver.Chrome('chromedriver.exe')
@@ -57,65 +51,66 @@ def app():
             results = soup.find(class_='list')
             job_elems = results.find_all('article', class_="jobTuple bgWhite br4 mb-8")
             for job_elem in job_elems:
-                job_URL = job_elem.find('a', class_='title fw500 ellipsis').get('href')                         # job listing URL
-                job_title = job_elem.find('a', class_='title fw500 ellipsis')                                   # Opening position title
-                company_URL = job_elem.find('a', class_='subTitle ellipsis fleft').get('href')                  # company's URL
-                company_title = job_elem.find('a', class_='subTitle ellipsis fleft')                            # company's title
+                job_URL = job_elem.find('a', class_='title fw500 ellipsis').get('href')  # job listing URL
+                job_title = job_elem.find('a', class_='title fw500 ellipsis')  # Opening position title
+                company_URL = job_elem.find('a', class_='subTitle ellipsis fleft').get('href')  # company's URL
+                company_title = job_elem.find('a', class_='subTitle ellipsis fleft')  # company's title
                 rating_span = job_elem.find('span', class_='starRating fleft dot')
-                if rating_span is None:                                                                         # job ratings
+                if rating_span is None:  # job ratings
                     continue
                 else:
                     Ratings = rating_span.text
                 review_span = job_elem.find('a', class_='reviewsCount ml-5 fleft blue-text')
 
-                if review_span is None:                                                                         # no of reviews
+                if review_span is None:  # no of reviews
                     continue
                 else:
                     reviews = review_span.text
 
-                Exp = job_elem.find('li', class_='fleft grey-text br2 placeHolderLi experience')                 # experience required
+                Exp = job_elem.find('li', class_='fleft grey-text br2 placeHolderLi experience')  # experience required
                 Exp_span = Exp.find('span', class_='ellipsis fleft fs12 lh16')
                 if Exp_span is None:
                     continue
                 else:
                     Experience = Exp_span.text
 
-                Sal = job_elem.find('li', class_='fleft grey-text br2 placeHolderLi salary')                      #offered salary
+                Sal = job_elem.find('li', class_='fleft grey-text br2 placeHolderLi salary')  # offered salary
                 Sal_span = Sal.find('span', class_='ellipsis fleft fs12 lh16')
                 if Sal_span is None:
                     continue
                 else:
                     Salary = Sal_span.text
 
-                Loc = job_elem.find('li', class_='fleft grey-text br2 placeHolderLi location')                     # location
+                Loc = job_elem.find('li', class_='fleft grey-text br2 placeHolderLi location')  # location
                 Loc_exp = Loc.find('span', class_='ellipsis fleft fs12 lh16')
                 if Loc_exp is None:
                     continue
                 else:
                     Location = Loc_exp.text
 
-                Hist = job_elem.find("div", ["type br2 fleft grey", "type br2 fleft green"])                        #posted ago
+                Hist = job_elem.find("div", ["type br2 fleft grey", "type br2 fleft green"])  # posted ago
                 Post_Hist = Hist.find('span', class_='fleft fw500')
                 if Post_Hist is None:
                     continue
                 else:
                     Post_History = Post_Hist.text
 
-                df = df.append(
-                    {'Job Title': job_title,'Job URL': job_URL, 'Company Title': company_title, 'Company URL': company_URL, 'Ratings': Ratings, 'Reviews': reviews,
-                     'Experience': Experience, 'Salary': Salary, 'Location': Location, 'Post History': Post_History},
-                    ignore_index=True)
-                st.write(df)
-    #             st.write(job_title.text)
-    #             st.write(job_URL)
-    #             st.write(company_title.text)
-    #             st.write(company_URL)
-    #             st.write(Ratings)
-    #             st.write(reviews)
-    #             st.write(Experience)
-    #             st.write(Salary)
-    #             st.write(Location)
-    #             st.write(Post_History)
+                # df = df.append(
+                #     {'Job Title': job_title, 'Job URL': job_URL, 'Company Title': company_title,
+                #      'Company URL': company_URL, 'Ratings': Ratings, 'Reviews': reviews,
+                #      'Experience': Experience, 'Salary': Salary, 'Location': Location, 'Post History': Post_History},
+                #     ignore_index=True)
+                # st.write(df)
+                st.write(job_title.text)
+                st.write(job_URL)
+                st.write(company_title.text)
+                st.write(company_URL)
+                st.write(Ratings)
+                st.write(reviews)
+                st.write(Experience)
+                st.write(Salary)
+                st.write(Location)
+                st.write(Post_History)
     except Exception as e:
         return e
 
